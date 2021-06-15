4585475662ab1503220c209c225ce0ec556236a4
Ahel Das Chatterjee <aheldc@gmail.com>
2021-06-15 15:12:51 +0530

    Skeleton of video conferencing and chatting application

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+const { ExpressPeerServer } = require("peer")
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+})
+
+app.use('/peerjs', peerServer)
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+
+app.get('/home/create',(req,res) => {
+  res.render('landing')
+})
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
+
+server.listen(process.env.PORT || 3000)
