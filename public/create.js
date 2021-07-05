cf0043f2b12b19ed7436c8e359656dc881f431f0
Ahel Das Chatterjee <aheldc@gmail.com>
2021-07-05 08:46:09 +0530

    Add alert box and other minor UI changes

--- public/create.js
+++ public/create.js
@@ -2,12 +2,21 @@ const create = document.getElementById('name')
 const submit = document.getElementById('submit')
 
 submit.addEventListener('click',function(e){
-    if(create.value === "")return
+    if(create.value === ""){
+        alert('Please enter your Name!!!!')
+        return
+    }
     const video = document.getElementsByName('video')
     const audio = document.getElementsByName('audio')
 
-    if(!video[0].checked && !video[1].checked)return
-    if(!audio[0].checked && !audio[1].checked)return
+    if(!video[0].checked && !video[1].checked){
+        alert('Please enter your preferred video settings!!!!')
+        return
+    }
+    if(!audio[0].checked && !audio[1].checked){
+        alert('Please enter your preferred audio settings!!!!')
+        return
+    }
 
     if(video[0].checked)window.sessionStorage.setItem('VideoSettings','On')
     else window.sessionStorage.setItem('VideoSettings','Off')
--- public/join.js
+++ public/join.js
@@ -3,15 +3,29 @@ const meetingId = document.getElementById('meeting-id')
 const submit = document.getElementById('submit')
 
 submit.addEventListener('click',function(e){
-    if(create.value === "")return
-    if(meetingId === "")return
+    if(create.value === ""){
+        alert('Please enter your Name!!!!')
+        return
+    }
+
+    if(meetingId === ""){
+        alert('Enter a valid Meeting Id!!!!')
+        return
+    }
 
     
     const video = document.getElementsByName('video')
     const audio = document.getElementsByName('audio')
 
-    if(!video[0].checked && !video[1].checked)return
-    if(!audio[0].checked && !audio[1].checked)return
+    if(!video[0].checked && !video[1].checked){
+        alert('Enter your preferred video settings!!!!')
+        return
+    }
+
+    if(!audio[0].checked && !audio[1].checked){
+        alert('Enter your preferred audio settings!!!!')
+        return
+    }
 
     if(video[0].checked)window.sessionStorage.setItem('VideoSettings','On')
     else window.sessionStorage.setItem('VideoSettings','Off')
